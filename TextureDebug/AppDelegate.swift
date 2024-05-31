b686cacfaf62f8822dfa21c76509ce2918eee29b
98prabowo <58059518+98prabowo@users.noreply.github.com>
2024-05-31 15:55:08 +0700

    Remove scene

--- TextureDebug/AppDelegate.swift
+++ TextureDebug/AppDelegate.swift
@@ -5,13 +5,17 @@
 //  Created by Dimas Agung Prabowo on 31/05/24.
 //
 
+import TextureSwiftSupport
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        _ = ASDisplayNode()
+        print("Success 🚀")
         return true
     }
 }
-
